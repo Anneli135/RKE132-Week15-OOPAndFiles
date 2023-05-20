c4cea3b0702cce03a638da7d0aa4ed80e5db2393
Anneli Hrginen <anneli.harginen@tktk.ee>
2023-05-20 13:19:43 +0300

    add week15 code

---
+++ Week15OOPAndFiles/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Reflection;
+using System.Runtime.Serialization.Formatters;
+
+List<Movie> myMovies = new List<Movie>();
+string[] data = GetDataFromMyFile();
+//ReadDataFrimArray(data);
+
+foreach (string line in data)
+{
+    string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+    Movie newMovie = new Movie(tempArray[0], tempArray[2], tempArray[1]);
+    myMovies.Add(newMovie);
+}
+
+foreach (Movie movie in myMovies)
+{
+    Console.WriteLine($"one of my favorite movies {movie.Title} was realsed in {movie.Year}.Rating: {movie.Rating}");
+}
+
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"C:\Users\User\Downloads\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach (string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+
+class Movie
+{
+    string title; //fields 
+    string year;
+    string rating;
+
+    public string Title
+    {
+           get { return title; }
+    }
+
+    public string Year
+    { get { return year; } }
+
+    public string Rating
+    { get { return rating; } }
+
+    public Movie(string _title, string _year, string _rating)
+    {
+    title = _title;
+        year = _year;
+        rating = _rating;
+    }
+}
